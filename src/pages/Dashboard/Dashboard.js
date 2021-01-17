1a4e1b2fc2682f6548a1739dbc164fbb26c2026a
Alan Alban <alan.alban10@gmail.com>
2021-01-17 12:55:47 -0500

    adding inner chart boxes to first left quadrant

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -5,7 +5,11 @@ function Dashboard(props) {
     return (
         <>
             <div className= {props.isOpen ? "Dashboard enabled" : "Dashboard" }>
-                <div className="Dashboard-Overview">A</div>
+                <div className="Dashboard-Overview">
+                    <div>circle 1</div>
+                    <div>circle 1</div>
+                    <div>circle 1</div>
+                </div>
                 <div className="Dashboard-Classifier">B</div>
                 <div className="Dashboard-Trends">C</div>
                 <div className="Dashboard-News">D</div>

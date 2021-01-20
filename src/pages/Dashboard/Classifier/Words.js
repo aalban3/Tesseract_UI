a50ce07ad4114695e1348c4d1b3e1552d29302f3
Alan Alban <alan.alban10@gmail.com>
2021-01-19 22:04:09 -0500

    separated dashboard items into new components

---
+++ src/pages/Dashboard/Classifier/Words.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as d3 from "d3";
+
+function Words() {
+    return (
+        <div>
+            NLP Overview
+        </div>
+    )
+}
+
+export default Words
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import './Dashboard.css'
-
+import Words from './Classifier/Words'
 function Dashboard(props) {
     return (
         <>
             <div className= {props.isOpen ? "Dashboard enabled" : "Dashboard" }>
-                <div className="Dashboard-Overview">
-                    <div>circle 1</div>
-                    <div>circle 1</div>
-                    <div>circle 1</div>
+                <div className="Dashboard-Overview">A</div>
+                <div className="Dashboard-Classifier">
+                    <Words />
+                    <Words />
+                    <Words />
                 </div>
-                <div className="Dashboard-Classifier">B</div>
                 <div className="Dashboard-Trends">C</div>
                 <div className="Dashboard-News">D</div>
             </div>
